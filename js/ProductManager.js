7dc2332eea628fd9359f704e58bc7d4ef506a933
JavyDigade <digade@hotmail.com.ar>
2023-01-19 00:04:04 -0300

    1

---
+++ js/ProductManager.js
@@ -0,0 +1,11 @@
+class ProductManager{
+    constructor(x){
+        this.title = 'producto prueba';
+        this.description='Este es un producto prueba';
+        this.price=200;
+        this.thumbnail='Sin imagen';
+        this.code='abc123';
+        this.stock=25;
+
+    }
+}
