125f7beb0b03c701caaae38107296895b095d3a3
JavyDigade <digade@hotmail.com.ar>
2023-01-23 01:02:54 -0300

    nueva correccion

--- js/ProductManager.js
+++ js/ProductManager.js
@@ -1,11 +1,67 @@
+let code = 0;
+
+generarIdUnico1 = () => { 
+    code= Math.random().toString(30).substring(2);           
+}
+
+generarIdUnico1();
+
 class ProductManager{
-    constructor(x){
-        this.title = 'producto prueba';
-        this.description='Este es un producto prueba';
-        this.price=200;
-        this.thumbnail='Sin imagen';
-        this.code='abc123';
-        this.stock=25;
+    constructor(title, description, price, thumbnail, code, stock){
+        this.title = title;
+        this.description=description;
+        this.price=price;
+        this.thumbnail=thumbnail;
+        this.code= code;
+        this.stock=stock;
 
     }
 }
+
+const product1 = new ProductManager
+    ("productoprueba 1","Este es un producto prueba", 200, 'Este es un producto prueba', code, 23)
+// Object.freeze(product1.code) si pudiera poner funciones en la creación ahorraria codigo, no me funciono;
+
+generarIdUnico1();
+const product2 = new ProductManager
+    ("productoprueba 2","Este es un producto prueba2", 350, 'Este es un producto prueba2', code, 20)
+
+ console.log(`este el el codigo id del producto ${product1.code}`);
+ console.log(`este el el codigo id del producto ${product2.code}`);
+
+ getProducts =() => {
+    console.log(product1, product2)
+    
+ }
+
+//  getProductsById = () => {
+//     let busqueda = prompt("¿Qué ID de producto buscas?");
+//         if (busqueda = product1.code) {
+//             alert(`este es tu producto ${product1.title}`)
+//         } else if (busqueda = product2.code) {
+//             alert(`este es tu producto ${product2.title}`)
+//         } else
+//             alert("error");
+//  } no funciono al no tener un dom para mostrar 
+let busqueda = product2.code
+
+getProductsById = () => {
+   
+        if (busqueda == product1.code) {
+            console.log(`este es tu producto ${product1.title}`)
+        } if (busqueda == product2.code) {
+            console.log(`este es tu producto ${product2.title}`)
+        } else
+            console.log("error, no existe ningún producto con ese id");
+ }
+
+getProducts();
+console.log("esta es la variable busqueda: " + busqueda)
+getProductsById();
+
+
+// function busXfind(busqueda) {
+//     return busqueda === product2.code;
+// }
+// const BusquedaXFind = product2.find(busqueda);
+// console.log(BusquedaXFind) no anda y esta fue la útima experimentacion
